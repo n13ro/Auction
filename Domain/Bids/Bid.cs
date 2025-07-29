caa375a2265831315d76a51530a5b4c5daba17a2
n13ro <danilbagisev472@gmail.com>
2025-07-30 00:34:24 +0700

    model bids
    A betting model has been created, and unnecessary files have been deleted

---
+++ Domain/Bids/Bid.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Common;
+using Domain.Lots;
+using Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Bids
+{
+    public class Bid : BaseEntity
+    {
+        public Lot CurrentLot { get; private set; }
+        public User User { get; private set; }
+        public long Amount { get; private set; }
+        public DateTime PlacedAt { get; private set; }
+
+        private Bid() { }
+        public Bid(User user, long amount, Lot currentLot)
+        {
+            if (amount >= currentLot.MinBet) Amount = amount;
+            else throw new Exception("Amount <=");
+
+            int Id = user.Id;
+            User = user;
+            CurrentLot = currentLot;
+            PlacedAt = DateTime.Now;
+        }
+
+
+    }
+}

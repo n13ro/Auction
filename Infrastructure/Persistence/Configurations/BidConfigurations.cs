6f50741b8eabb30479211ca9044dca364a25a14d
n13ro <danilbagisev472@gmail.com>
2025-07-30 00:36:57 +0700

    edit Infrastructure
    add cfg in Infrastructure

---
+++ Infrastructure/Persistence/Configurations/BidConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class BidConfigurations : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Bids;
+using Domain.Lots;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Lot> Lots { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}

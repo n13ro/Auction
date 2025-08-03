12616ce481bea64de268f921eb1fcf561f02ac55
unknown <levin100791@gmail.com>
2025-08-03 22:06:19 +0700

    header

--- auction_frontend/src/app/app.ts
+++ auction_frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LotMap } from './lot-map/lot-map'
+import { Header } from './header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LotMap],
+  imports: [RouterOutlet, LotMap, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

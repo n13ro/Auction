3cc96a67b95d1ff8aaaf2231ce1431ea7dc2bd23
unknown <levin100791@gmail.com>
2025-08-09 05:06:12 +0700

    invisible_menu_on_image_click

--- auction_frontend/src/app/header/header.ts
+++ auction_frontend/src/app/header/header.ts
@@ -8,12 +8,20 @@ import { Router } from '@angular/router';
   styleUrl: './header.scss'
 })
 export class Header {
+  image_up = "images/arrow_up.svg";
+  image_down = "images/arrow_down.svg"
+  currentImage = this.image_up;
   @Output() show = new EventEmitter<void>();
   constructor(
     private router: Router
   ){}
 
+  toggleImage() {
+    this.currentImage = this.currentImage === this.image_up ? this.image_down : this.image_up;
+  }
+
   Login(){
     this.router.navigate(['/login']);
   }
 }
+
--- auction_frontend/src/app/main/main.ts
+++ auction_frontend/src/app/main/main.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LotMap } from '../lot-map/lot-map';
-import { Header } from '../header/header';
+import { Header} from '../header/header';
 import { TimeOutLot } from '../time-out-lot/time-out-lot';
 import { PriceLot } from '../price-lot/price-lot';
 import { NgIf } from '@angular/common';
@@ -13,4 +13,8 @@ import { NgIf } from '@angular/common';
 })
 export class Main {
   menuVisible: boolean = true;
+  seeMenu(){
+    this.menuVisible = !this.menuVisible
+    
+  }
 }

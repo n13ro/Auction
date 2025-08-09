65533c42be8053cab704085323f6013720959b32
n13ro <danilbagisev472@gmail.com>
2025-08-10 01:01:48 +0700

    hashed
    create methonds hashing value

---
+++ Shared/Encryption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    class Encryption
+    {
+        public static string HashedValue(string value)
+        {
+            var hashedSha256 = SHA256.HashData(Encoding.UTF8.GetBytes(value));
+            return Convert.ToString(hashedSha256);
+        }
+        
+
+        public static bool IsEqualHashValues(string value1, string value2)
+        {
+            HashedValue(value1);
+            return value1.Equals(value2);
+        }
+    }
+}

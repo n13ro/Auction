0928e5c24c1bcdda8065dc30f4e01ee7c11d0a27
unknown <levin100791@gmail.com>
2025-08-10 15:26:04 +0700

    bet data from array

--- auction_frontend/src/app/lot/lot.ts
+++ auction_frontend/src/app/lot/lot.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Bet } from '../bet/bet';
 import { NgIf } from '@angular/common';
-
+interface bet{
+    name:string;
+    date:string;
+    amount:string;
+  }
 @Component({
   selector: 'app-lot',
   imports: [Header, Bet, NgIf],
   templateUrl: './lot.html',
   styleUrl: './lot.scss'
 })
+
 export class Lot {
+  bets:Array<bet> = [
+    {name: "Ivan", date: "11:11:12", amount: "5.000.000p." },
+    {name: "Olga", date: "11:11:15", amount: "6.000.000p." },
+    {name: "Max Maxbetov", date: "11:11:25", amount: "7.500.000p." }
+  ];
   betting:boolean = false;
   showBet(){
     this.betting = true;

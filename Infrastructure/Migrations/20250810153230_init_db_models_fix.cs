606ee21a989511b47bb9ec619844e7de4c03af9c
n13ro <danilbagisev472@gmail.com>
2025-08-10 22:58:26 +0700

    fix megre

--- Infrastructure/Migrations/20250810153230_init_db_models_fix.cs
+++ Infrastructure/Migrations/20250810153230_init_db_models_fix.cs
@@ -7,7 +7,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class init_db_models : Migration
+    public partial class init_db_models_fix : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
